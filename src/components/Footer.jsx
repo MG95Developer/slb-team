6e9359c803e200c7a98875d3fffd7e2032f48115
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2022-12-05 19:35:21 +0000

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ExternalLink } from 'react-external-link'
 
 const Footer = () =>
 {
     return (
-        <div id="footer">Footer</div>
+        <div id="footer">
+            <p style={{ fontSize: '14px', padding: '1em 0', opacity: '.8' }}>No Rights Reserved. This is just a personal project for my Portfolio.</p>
+            <ExternalLink href="https://www.slbenfica.pt/en-us/" target="_blank">
+                Credits: Visit Official Sport Lisboa e Benfica website here.
+            </ExternalLink>
+        </div>
     )
 }
 
